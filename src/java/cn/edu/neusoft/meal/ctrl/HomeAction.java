8db4e4087056bb8693aab1eae2c811e554f1e7b2
kkai <zmkkai@gmail.com>
2016-12-08 14:16:12 +0800

    ce

--- src/java/cn/edu/neusoft/meal/ctrl/HomeAction.java
+++ src/java/cn/edu/neusoft/meal/ctrl/HomeAction.java
@@ -1,20 +1,20 @@
 package cn.edu.neusoft.meal.ctrl;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
+import cn.edu.neusoft.meal.domain.User;
+import cn.edu.neusoft.meal.service.LetterService;
+import cn.edu.neusoft.meal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import cn.edu.neusoft.meal.domain.User;
-import cn.edu.neusoft.meal.service.LetterService;
-import cn.edu.neusoft.meal.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeAction {
 	//nihao
+	//shishi
 	@Autowired
 	private LetterService letterService;
 	@Autowired
